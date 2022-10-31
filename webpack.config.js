bf0226596acb631c5eaf34e5ef074dbab0e0f880
Shubham Tiwari <shubhmtiwri00@gmail.com>
2022-10-31 17:11:33 +0530

    applied the env files using dotenv-webpack

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
     optimization: {
         minimizer: [
             // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
-            // `...`,
+            `...`,
             new CssMinimizerPlugin(),
         ],
         minimize: true //for development mode
